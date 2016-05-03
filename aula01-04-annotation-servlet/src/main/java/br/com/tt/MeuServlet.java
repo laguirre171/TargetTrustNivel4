4480dc8814c0c120e0567f92e1502959cbb3d989
sala03 <sala03@targettrust.local>
2016-05-02 22:44:48 -0300

    Aula 1

--- aula01-04-annotation-servlet/src/main/java/br/com/tt/MeuServlet.java
+++ aula01-04-annotation-servlet/src/main/java/br/com/tt/MeuServlet.java
@@ -3,10 +3,12 @@ package br.com.tt;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet(name = "minha-servlet", urlPatterns={"/teste","/pagina/*",})
 public class MeuServlet extends HttpServlet {
 
 	/**
@@ -16,6 +18,7 @@ public class MeuServlet extends HttpServlet {
 	
 	public void service(HttpServletRequest request, HttpServletResponse response) throws IOException{
 		PrintWriter writer = response.getWriter();
+		writer.println("<h1>teste da minha servlet anotada");
 		
 	}
 	
