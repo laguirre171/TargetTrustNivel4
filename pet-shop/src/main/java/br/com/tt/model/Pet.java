4e5c4dfb593d79e8d99daff1eee955e357a5b18b
sala03 <sala03@targettrust.local>
2016-05-05 22:25:47 -0300

    Aplicação Pet-Shop

---
+++ pet-shop/src/main/java/br/com/tt/model/Pet.java
@@ -0,0 +1,52 @@
+package br.com.tt.model;
+
+public class Pet {
+	private String nome;
+	private String cor;
+	private String raca;
+	private String especie;
+	private String sexo;
+	
+	public String getRaca() {
+		return raca;
+	}
+
+	public void setRaca(String raca) {
+		this.raca = raca;
+	}
+
+	public String getEspecie() {
+		return especie;
+	}
+
+	public void setEspecie(String especie) {
+		this.especie = especie;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+
+}
---
+++ pet-shop/src/main/java/br/com/tt/pet/bean/ManagedBean/PetBean.java
@@ -0,0 +1,55 @@
+package br.com.tt.pet.bean.ManagedBean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import br.com.tt.model.Pet;
+import br.com.tt.pet.util.UtilMessage;
+
+@ManagedBean
+@SessionScoped
+public class PetBean {
+	
+	private Pet pet = new Pet();
+	private List<Pet> pets = new ArrayList<Pet>();
+		
+	public Pet getPet() {
+		return pet;
+	}
+
+
+	public void setPet(Pet pet) {
+		this.pet = pet;
+	}
+
+
+	public void salvar(){
+		pets.add(pet);
+		pet = new Pet();
+		
+		String detail = "Pet Salvo com sucesso";
+		String summary = "Pet Salvo";
+		UtilMessage.info(detail, summary);
+		//UtilMessage.error(detail, summary);
+		System.out.println(pets.size());
+	}
+
+
+	public List<Pet> getPets() {
+		return pets;
+	}
+
+
+
+	/*
+	private void MessageInfo(String detail, String summary) {
+		FacesContext.getCurrentInstance().addMessage("form-message", new FacesMessage(FacesMessage.SEVERITY_INFO, summary, detail));
+	}
+	*/
+
+	
+	
+	
+}
